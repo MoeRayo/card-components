0461f996a359bf4fd12f5b93745743c6134d9b0a
MoeRayo <moronfolu.motunrayo1@gmail.com>
2021-05-09 05:03:06 +0100

    fea: fixing of the wrappers width

--- src/components/CardsComponent.vue
+++ src/components/CardsComponent.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="container w-3/4 mx-auto ">
-        <div class="flex md:flex-row lg:w-9/12 mx-auto flex-col p-3 ">
+        <div class="flex md:flex-row lg:w-10/12 mx-auto card-wrapper flex-col p-3 ">
             <div class="flex flex-col single-card p-9 rounded" v-for="content in contents" :key="content.id">
                     <div class="my-4 vehicle" >
                         <img :src="getImage(content.logo)" alt="">
                     </div>
-                   <h1 class="text-4xl my-6"> {{content.type}}</h1>
+                   <h1 class="text-4xl my-6 h-2/5"> {{content.type}}</h1>
                    <p class="mt-4 mb-20 text-sm cards-para"> {{content.description}}</p>
                    <div>
                     <button class="text-xs py-3 px-5 text-white border-white-500 border-2 rounded-full bottom-0 self-end hover:text-current" type="button">Learn More</button>
